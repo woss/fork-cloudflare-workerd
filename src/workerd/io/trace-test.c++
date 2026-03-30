Ankcorn/workerd

Add preview metadata to buffered tail traces

src/workerd/io/trace-test.c++
@@ -577,12 +577,33 @@ KJ_TEST("Read/Write TailEvent with Multiple Attributes") {
   KJ_ASSERT(attrs2[1].name == "bar"_kj);
 }
 
+KJ_TEST("Trace with Preview") {
+  auto trace = kj::refcounted<Trace>(kj::str("test-stable-id"), kj::str("test-script"),
+      kj::none,  // scriptVersion
+      kj::str("test-namespace"), kj::str("test-script-id"),
+      kj::Array<kj::String>(),  // scriptTags
+      kj::str("test-entrypoint"), ExecutionModel::STATELESS,
+      TracePreview(kj::str("63bafce9179948688866bb22268eb1c6"), kj::str("feature-my-branch"),
+          kj::str("feature/my-branch")));
+
+  capnp::MallocMessageBuilder builder;
+  auto traceBuilder = builder.initRoot<rpc::Trace>();
+  trace->copyTo(traceBuilder);
+
+  auto trace2 = kj::refcounted<Trace>(traceBuilder.asReader());
+  auto& preview = KJ_ASSERT_NONNULL(trace2->preview);
+  KJ_ASSERT(preview.id == "63bafce9179948688866bb22268eb1c6"_kj);
+  KJ_ASSERT(preview.slug == "feature-my-branch"_kj);
+  KJ_ASSERT(preview.name == "feature/my-branch"_kj);
+}
+
 KJ_TEST("Trace with Durable Object ID") {
   auto trace = kj::refcounted<Trace>(kj::str("test-stable-id"), kj::str("test-script"),
       kj::none,  // scriptVersion
       kj::str("test-namespace"), kj::str("test-script-id"),
       kj::Array<kj::String>(),  // scriptTags
       kj::str("test-entrypoint"), ExecutionModel::DURABLE_OBJECT,
+      kj::none,  // preview
       kj::str("abc123def456")  // durableObjectId
   );
 

src/workerd/io/trace.h
@@ -423,6 +423,21 @@ struct EmailEventInfo final {
 };
 
 // Describes a buffered tail worker request
+struct TracePreview final {
+  explicit TracePreview(kj::String id, kj::String slug, kj::String name);
+  TracePreview(rpc::Trace::Preview::Reader reader);
+  TracePreview(TracePreview&&) = default;
+  TracePreview& operator=(TracePreview&&) = default;
+  KJ_DISALLOW_COPY(TracePreview);
+
+  kj::String id;
+  kj::String slug;
+  kj::String name;
+
+  void copyTo(rpc::Trace::Preview::Builder builder) const;
+  TracePreview clone() const;
+};
+
 struct TraceEventInfo final {
   struct TraceItem;
 
@@ -885,6 +900,7 @@ class Trace final: public kj::Refcounted {
       kj::Array<kj::String> scriptTags,
       kj::Maybe<kj::String> entrypoint,
       ExecutionModel executionModel,
+      kj::Maybe<tracing::TracePreview> preview = kj::none,
       kj::Maybe<kj::String> durableObjectId = kj::none);
   Trace(rpc::Trace::Reader reader);
   ~Trace() noexcept(false);
@@ -908,6 +924,7 @@ class Trace final: public kj::Refcounted {
   kj::Array<kj::String> scriptTags;
   kj::Maybe<kj::Array<tracing::Attribute>> tailAttributes;
   kj::Maybe<kj::String> entrypoint;
+  kj::Maybe<tracing::TracePreview> preview;
   kj::Maybe<kj::String> durableObjectId;
 
   kj::Vector<tracing::Log> logs;

woss/fork-cloudflare-workerd

src/workerd/io/trace-test.c++
@@ -577,6 +577,27 @@ KJ_TEST("Read/Write TailEvent with Multiple Attributes") {
   KJ_ASSERT(attrs2[1].name == "bar"_kj);
 }
 
+KJ_TEST("Trace with Preview") {
+  auto trace = kj::refcounted<Trace>(kj::str("test-stable-id"), kj::str("test-script"),
+      kj::none,  // scriptVersion
+      kj::str("test-namespace"), kj::str("test-script-id"),
+      kj::Array<kj::String>(),  // scriptTags
+      kj::str("test-entrypoint"), ExecutionModel::STATELESS,
+      kj::none,  // durableObjectId
+      TracePreview(kj::str("63bafce9179948688866bb22268eb1c6"), kj::str("feature-my-branch"),
+          kj::str("feature/my-branch")));
+
+  capnp::MallocMessageBuilder builder;
+  auto traceBuilder = builder.initRoot<rpc::Trace>();
+  trace->copyTo(traceBuilder);
+
+  auto trace2 = kj::refcounted<Trace>(traceBuilder.asReader());
+  auto& preview = KJ_ASSERT_NONNULL(trace2->preview);
+  KJ_ASSERT(preview.id == "63bafce9179948688866bb22268eb1c6"_kj);
+  KJ_ASSERT(preview.slug == "feature-my-branch"_kj);
+  KJ_ASSERT(preview.name == "feature/my-branch"_kj);
+}
+
 KJ_TEST("Trace with Durable Object ID") {
   auto trace = kj::refcounted<Trace>(kj::str("test-stable-id"), kj::str("test-script"),
       kj::none,  // scriptVersion

src/workerd/io/trace.h
@@ -432,6 +432,21 @@ struct EmailEventInfo final {
 };
 
 // Describes a buffered tail worker request
+struct TracePreview final {
+  explicit TracePreview(kj::String id, kj::String slug, kj::String name);
+  TracePreview(rpc::Trace::TracePreviewInfo::Reader reader);
+  TracePreview(TracePreview&&) = default;
+  TracePreview& operator=(TracePreview&&) = default;
+  KJ_DISALLOW_COPY(TracePreview);
+
+  kj::String id;
+  kj::String slug;
+  kj::String name;
+
+  void copyTo(rpc::Trace::TracePreviewInfo::Builder builder) const;
+  TracePreview clone() const;
+};
+
 struct TraceEventInfo final {
   struct TraceItem;
 
@@ -895,7 +910,8 @@ class Trace final: public kj::Refcounted {
       kj::Array<kj::String> scriptTags,
       kj::Maybe<kj::String> entrypoint,
       ExecutionModel executionModel,
-      kj::Maybe<kj::String> durableObjectId = kj::none);
+      kj::Maybe<kj::String> durableObjectId = kj::none,
+      kj::Maybe<tracing::TracePreview> preview = kj::none);
   Trace(rpc::Trace::Reader reader);
   ~Trace() noexcept(false);
   KJ_DISALLOW_COPY_AND_MOVE(Trace);
@@ -918,6 +934,7 @@ class Trace final: public kj::Refcounted {
   kj::Array<kj::String> scriptTags;
   kj::Maybe<kj::Array<tracing::Attribute>> tailAttributes;
   kj::Maybe<kj::String> entrypoint;
+  kj::Maybe<tracing::TracePreview> preview;
   kj::Maybe<kj::String> durableObjectId;
 
   kj::Vector<tracing::Log> logs;